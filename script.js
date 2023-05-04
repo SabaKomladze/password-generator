b4f0f373f2be1de6531b66fed1d8c9d93df88a38
Saba Komladze <121091520+SabaKomladze@users.noreply.github.com>
2023-05-04 21:24:22 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,40 +3,33 @@ const slider = document.getElementsByClassName("range")[0];
 const bar = document.querySelectorAll(".box");
 const strength = document.getElementsByClassName("med")[0];
 const checkbox = document.querySelectorAll(".check");
-let checkedCount = 0;
-function countCheckedCheckboxes() {
-  for (let i = 0; i < checkbox.length; i++) {
-    if (checkbox[i].checked) {
-      checkedCount++;
-    }
-  }
-  return checkedCount;
-}
-for (let i = 0; i < checkbox.length; i++) {
-  checkbox[i].addEventListener("change", countCheckedCheckboxes);
-}
+const upper = document.getElementById('upper');
+const lower = document.getElementById('lower');
+const number = document.getElementById('number');
+const symbol = document.getElementById('symbol');
 
-checkbox.addEventListener("click",countCheckedCheckboxes) (
+
+slider.oninput=function (){
   sliderValue.innerHTML = this.value;
-  if (slider.value < 7 || checkedCount <= 1) {
+  if (slider.value < 7 ) {
     strength.innerHTML = "TOO WEAK";
     for (let j = 0; j < bar.length; j++) {
       bar[j].style.backgroundColor = "#18171F";
       for (let i = 0; i < bar.length - 3; i++) {
-        bar[i].style.backgroundColor = "#F8CD65";
+        bar[i].style.backgroundColor = "#F64A4A";
         bar[i].style.border = "none";
       }
     }
-  } else if ((slider.value > 7 && slider.value < 10) || checkedCount === 2) {
+  } else if (slider.value > 7 && slider.value < 10) {
     strength.innerHTML = "MEDIUM";
     for (let j = 0; j < bar.length; j++) {
       bar[j].style.backgroundColor = "#18171F";
       for (let i = 0; i < bar.length - 2; i++) {
-        bar[i].style.backgroundColor = "#F8CD65";
+        bar[i].style.backgroundColor = "#FB7C58";
         bar[i].style.border = "none";
       }
     }
-  } else if ((slider.value > 10 && slider.value < 13) || checkedCount === 3) {
+  } else if (slider.value > 10 && slider.value < 13) {
     strength.innerHTML = "STRONG";
     for (let j = 0; j < bar.length; j++) {
       bar[j].style.backgroundColor = "#18171F";
@@ -45,14 +38,16 @@ checkbox.addEventListener("click",countCheckedCheckboxes) (
         bar[i].style.border = "none";
       }
     }
-  } else if (slider.value > 13 || checkedCount === 4) {
+  } else if (slider.value > 13) {
     strength.innerHTML = "VERY STRONG";
     for (let j = 0; j < bar.length; j++) {
       bar[j].style.backgroundColor = "#18171F";
       for (let i = 0; i < bar.length; i++) {
-        bar[i].style.backgroundColor = "#F8CD65";
+        bar[i].style.backgroundColor = "#A4FFAF";
         bar[i].style.border = "none";
       }
     }
   }
-);
+}
+
+
