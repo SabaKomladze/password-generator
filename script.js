a9976cf41333486576a4a0f2742e898cf677b08c
Saba Komladze <sabakomladze2@gmail.com>
2023-05-04 22:57:27 +0400

    Update script.js

--- script.js
+++ script.js
@@ -7,47 +7,54 @@ const upper = document.getElementById('upper');
 const lower = document.getElementById('lower');
 const number = document.getElementById('number');
 const symbol = document.getElementById('symbol');
-
-
+strength.innerHTML = "";
 slider.oninput=function (){
   sliderValue.innerHTML = this.value;
-  if (slider.value < 7 ) {
-    strength.innerHTML = "TOO WEAK";
-    for (let j = 0; j < bar.length; j++) {
-      bar[j].style.backgroundColor = "#18171F";
-      for (let i = 0; i < bar.length - 3; i++) {
-        bar[i].style.backgroundColor = "#F64A4A";
-        bar[i].style.border = "none";
-      }
-    }
-  } else if (slider.value > 7 && slider.value < 10) {
-    strength.innerHTML = "MEDIUM";
-    for (let j = 0; j < bar.length; j++) {
-      bar[j].style.backgroundColor = "#18171F";
-      for (let i = 0; i < bar.length - 2; i++) {
-        bar[i].style.backgroundColor = "#FB7C58";
-        bar[i].style.border = "none";
-      }
-    }
-  } else if (slider.value > 10 && slider.value < 13) {
-    strength.innerHTML = "STRONG";
-    for (let j = 0; j < bar.length; j++) {
-      bar[j].style.backgroundColor = "#18171F";
-      for (let i = 0; i < bar.length - 1; i++) {
-        bar[i].style.backgroundColor = "#F8CD65";
-        bar[i].style.border = "none";
-      }
-    }
-  } else if (slider.value > 13) {
-    strength.innerHTML = "VERY STRONG";
-    for (let j = 0; j < bar.length; j++) {
-      bar[j].style.backgroundColor = "#18171F";
-      for (let i = 0; i < bar.length; i++) {
-        bar[i].style.backgroundColor = "#A4FFAF";
-        bar[i].style.border = "none";
-      }
-    }
+let selectedCount = 0;
+for (let i = 0; i < checkbox.length; i++) {
+  if (checkbox[i].checked) {
+    selectedCount++;
   }
 }
 
 
+if (selectedCount === 1) {
+  strength.innerHTML = "TOO WEAK";
+  for (let j = 0; j < bar.length; j++) {
+    bar[j].style.backgroundColor = "#18171F";
+    for (let i = 0; i < bar.length - 3; i++) {
+      bar[i].style.backgroundColor = "#F64A4A";
+      bar[i].style.border = "none";
+    }
+  }
+} else if (selectedCount === 2) {
+  strength.innerHTML = "WEAK";
+  for (let j = 0; j < bar.length; j++) {
+    bar[j].style.backgroundColor = "#18171F";
+    for (let i = 0; i < bar.length - 2; i++) {
+      bar[i].style.backgroundColor = "#FB7C58";
+      bar[i].style.border = "none";
+    }
+  }
+} else if (selectedCount === 3) {
+  strength.innerHTML = "MEDIUM";
+  for (let j = 0; j < bar.length; j++) {
+    bar[j].style.backgroundColor = "#18171F";
+    for (let i = 0; i < bar.length - 1; i++) {
+      bar[i].style.backgroundColor = "#F8CD65";
+      bar[i].style.border = "none";
+    }
+  }
+} else if (selectedCount === 4) {
+  strength.innerHTML = "STRONG";
+  for (let j = 0; j < bar.length; j++) {
+    bar[j].style.backgroundColor = "#18171F";
+    for (let i = 0; i < bar.length; i++) {
+      bar[i].style.backgroundColor = "#A4FFAF";
+      bar[i].style.border = "none";
+    }
+  }
+} else {
+  strength.innerHTML = "";
+}
+}
